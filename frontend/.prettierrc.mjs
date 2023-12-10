c3470433fdf3221bed9d554d7acd4faac4b91a4e
JJJJJJack <97jack@live.it>
2023-12-10 04:09:36 +0100

    react added + pretier format changes

--- frontend/.prettierrc.mjs
+++ frontend/.prettierrc.mjs
@@ -1,24 +1,21 @@
 export default {
-  plugins: [
-    'prettier-plugin-astro',
-    'prettier-plugin-tailwindcss',
-  ],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
-      files: '*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+      files: "*.{astro,md,mdx,svelte,js,ts,jsx,tsx}",
       options: {
-        parser: 'astro',
+        parser: "astro",
       },
     },
   ],
-  "semi": true,
-  "trailingComma": "es5",
-  "singleQuote": false,
-  "printWidth": 120,
-  "useTabs": false,
-  "tabWidth": 2,
-  "quoteProps": "as-needed",
-  "bracketSpacing": true,
-  "bracketSameLine": false,
-  "arrowParens": "avoid"
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
 };
