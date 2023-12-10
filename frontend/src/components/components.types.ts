15ed22d21b54a72f3dab99e1e90ae7f7f166d1bd
JJJJJJack <97jack@live.it>
2023-12-10 04:08:05 +0100

    EDIT: better types readability

--- frontend/src/components/components.types.ts
+++ frontend/src/components/components.types.ts
@@ -1,10 +1,10 @@
-export type GoResponse = {
+export type GoApiResponse = {
     data: any;
     error: string;
 }
+export type GoApiErr = string
 
 export type Architecture = 1 | 2;
-
 export type Process = {
     name: string;
     pid: number;
