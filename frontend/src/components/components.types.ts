0c4409c9694c0b6674c20fe6a84332593f21b0aa
JJJJJJack <97jack@live.it>
2023-12-11 03:15:39 +0100

    DRAFT: 1. SVGs 2. table align textand content 3. table padding removed

--- frontend/src/components/components.types.ts
+++ frontend/src/components/components.types.ts
@@ -1,12 +1,12 @@
-export type GoApiResponse = {
-    data: any;
-    error: string;
-}
-export type GoApiErr = string
+export type GoApiResponse<T> = {
+  data: T;
+  error: string;
+};
+export type GoApiErr = string;
 
 export type Architecture = 1 | 2;
 export type Process = {
-    name: string;
-    pid: number;
-    arch: Architecture;
-}
+  name: string;
+  pid: number;
+  arch: Architecture;
+};
