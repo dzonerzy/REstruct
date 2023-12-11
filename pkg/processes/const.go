807367fdfb5797c0ec659f8d39b29a9ca50d157e
dzonerzy <dzonerzy@gmail.com>
2023-12-11 13:23:48 +0100

    icon support

--- pkg/processes/const.go
+++ pkg/processes/const.go
@@ -15,12 +15,34 @@ const (
 	// INVALID_HANDLE_VALUE is a handle value that is used to indicate
 	// an invalid handle.
 	INVALID_HANDLE_VALUE = syscall.InvalidHandle
-	// SHGFI_TYPENAME is a flag for SHGetFileInfo that returns the
-	// type name.
-	SHGFI_TYPENAME = 0x000000400
-	// SHGFI_ICON is a flag for SHGetFileInfo that returns the icon.
+	// BI_RGB is a flag for CreateDIBSection that specifies that the
+	// bitmap is in uncompressed red green blue (RGB) format that is
+	// not compressed and does not use color masks.
+	BI_RGB = 0
+	// GHND is a flag for GlobalAlloc that combines GMEM_MOVEABLE and GMEM_ZEROINIT.
+	GHND = 0x0042
+	// FILE_ATTRIBUTE_NORMAL is a flag for CreateFile that specifies
+	// that the file is being opened or created for asynchronous I/O.
+	FILE_ATTRIBUTE_NORMAL = 0x00000080
+	// SHGFI_USEFILEATTRIBUTES is a flag for SHGetFileInfo that
+	// specifies that the dwFileAttributes member of the SHFILEINFO
+	// structure is valid.
+	SHGFI_USEFILEATTRIBUTES = 0x000000010
+	// SHGFI_ICON is a flag for SHGetFileInfo that specifies that the
+	// hIcon member of the SHFILEINFO structure is valid.
 	SHGFI_ICON = 0x000000100
-	// SHGFI_DISPLAYNAME is a flag for SHGetFileInfo that returns the
-	// display name.
-	SHGFI_DISPLAYNAME = 0x000000200
+	// SHGFI_SMALLICON is a flag for SHGetFileInfo that specifies that
+	// the small icon should be extracted.
+	SHGFI_SMALLICON = 0x000000001
+	// SHGFI_LARGEICON is a flag for SHGetFileInfo that specifies that
+	// the large icon should be extracted.
+	SHGFI_LARGEICON = 0x000000000
+	// DI_NORMAL is a flag for DrawIconEx that specifies that the icon
+	// is drawn using the width and height specified by the system
+	// metric values for icons.
+	DI_NORMAL = 0x0003
+	// DIB_RGB_COLORS is a flag for GetDIBits that specifies that the
+	// color table should consist of arrays of red, green, and blue
+	// intensities.
+	DIB_RGB_COLORS = 0
 )
--- pkg/processes/types.go
+++ pkg/processes/types.go
@@ -9,25 +9,143 @@ const (
 )
 
 type Process struct {
-	Name string       `json:"name"`
-	Pid  int          `json:"pid"`
-	Arch Architecture `json:"arch"`
+	Description string       `json:"description"`
+	Name        string       `json:"name"`
+	Icon        string       `json:"icon"`
+	Pid         int          `json:"pid"`
+	Arch        Architecture `json:"arch"`
 }
 
 /*
-	typedef struct _SHFILEINFOA {
+	typedef struct LANGANDCODEPAGE {
+		WORD wLanguage;
+		WORD wCodePage;
+	} *lpTranslate;
+*/
+
+type LANGANDCODEPAGE struct {
+	wLanguage uint16
+	wCodePage uint16
+}
+
+/*
+	typedef struct _ICONINFO {
+	BOOL    fIcon;
+	DWORD   xHotspot;
+	DWORD   yHotspot;
+	HBITMAP hbmMask;
+	HBITMAP hbmColor;
+	} ICONINFO;
+*/
+
+type ICONINFO struct {
+	fIcon    int32
+	xHotspot uint32
+	yHotspot uint32
+	hbmMask  uintptr
+	hbmColor uintptr
+}
+
+/*
+	typedef struct tagBITMAP {
+	LONG   bmType;
+	LONG   bmWidth;
+	LONG   bmHeight;
+	LONG   bmWidthBytes;
+	WORD   bmPlanes;
+	WORD   bmBitsPixel;
+	LPVOID bmBits;
+	} BITMAP, *PBITMAP, *NPBITMAP, *LPBITMAP;
+*/
+
+type BITMAP struct {
+	bmType       int32
+	bmWidth      int32
+	bmHeight     int32
+	bmWidthBytes int32
+	bmPlanes     uint16
+	bmBitsPixel  uint16
+	bmBits       uintptr
+}
+
+/*
+	typedef struct tagBITMAPINFOHEADER {
+	DWORD biSize;
+	LONG  biWidth;
+	LONG  biHeight;
+	WORD  biPlanes;
+	WORD  biBitCount;
+	DWORD biCompression;
+	DWORD biSizeImage;
+	LONG  biXPelsPerMeter;
+	LONG  biYPelsPerMeter;
+	DWORD biClrUsed;
+	DWORD biClrImportant;
+	} BITMAPINFOHEADER, *LPBITMAPINFOHEADER, *PBITMAPINFOHEADER;
+*/
+
+type BITMAPINFOHEADER struct {
+	biSize          uint32
+	biWidth         int32
+	biHeight        int32
+	biPlanes        uint16
+	biBitCount      uint16
+	biCompression   uint32
+	biSizeImage     uint32
+	biXPelsPerMeter int32
+	biYPelsPerMeter int32
+	biClrUsed       uint32
+	biClrImportant  uint32
+}
+
+/*
+	typedef struct tagRGBQUAD {
+	BYTE rgbBlue;
+	BYTE rgbGreen;
+	BYTE rgbRed;
+	BYTE rgbReserved;
+	} RGBQUAD;
+*/
+
+type RGBQUAD struct {
+	rgbBlue     byte
+	rgbGreen    byte
+	rgbRed      byte
+	rgbReserved byte
+}
+
+/*
+	typedef struct tagBITMAPINFO {
+	BITMAPINFOHEADER bmiHeader;
+	RGBQUAD          bmiColors[1];
+	} BITMAPINFO, *LPBITMAPINFO, *PBITMAPINFO;
+*/
+
+type BITMAPINFO struct {
+	bmiHeader BITMAPINFOHEADER
+	bmiColors [1]RGBQUAD
+}
+
+/*
+	typedef struct _SHFILEINFOW {
 	HICON hIcon;
 	int   iIcon;
 	DWORD dwAttributes;
-	CHAR  szDisplayName[MAX_PATH];
-	CHAR  szTypeName[80];
-	} SHFILEINFOA;
+	WCHAR szDisplayName[MAX_PATH];
+	WCHAR szTypeName[80];
+	} SHFILEINFOW;
 */
 
-type SHFILEINFOA struct {
+type SHFILEINFOW struct {
 	hIcon         uintptr
 	iIcon         int32
 	dwAttributes  uint32
 	szDisplayName [260]uint16
 	szTypeName    [80]uint16
+}
+
+type CustomIconInfo struct {
+	Width        int32
+	Height       int32
+	BitsPerPixel uint16
 }
