0c09cccbdfe1295811d0e8d7ef290b3a0d80ca2b
JJJJJJack <97jack@live.it>
2023-12-23 12:56:45 +0100

    Delete unused components A and B

--- frontend/src/components/A.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "flowbite-react";
-import { useNavigate } from "react-router-dom";
-
-export default function A() {
-  const navigate = useNavigate();
-
-  return <Button onClick={() => navigate("/b")}>to B</Button>;
-}
