8e1c52d0d0a276a3742ed05e7c98010fa4a24636
JJJJJJack <97jack@live.it>
2023-12-23 13:03:32 +0100

    EDIT: colors

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -13,8 +13,8 @@ export default function Home() {
   return (
     <div className="flex h-full items-center">
       <h1 className="text-3xl font-bold text-white">
-        Welcome to <span className="text-red-200">RE</span>
-        <span className="text-red-500">struct</span>
+        Welcome to <span className="text-teal-100">RE</span>
+        <span className="text-teal-600">struct</span>
       </h1>
     </div>
   );
