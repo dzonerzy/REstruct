44064a6c5a77e9766414958ac1d589024c5fa246
JJJJJJack <97jack@live.it>
2023-12-23 01:23:55 +0100

    REFACTOR: - index.css break scrollbar - Layout skeleton - MainContent - Sidebar

--- frontend/src/components/MainContent/MainContent.tsx
+++ frontend/src/components/MainContent/MainContent.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 import "./MainContent.css";
 
 /** Contains Outlet (the "slot" of astro for react-router-dom) */
-export default function MainContent() {
+export default function MainContent({ className }) {
   return (
-    <div className="height-less-footer ml-[128px] p-4 pb-8">
+    <div className={className}>
       <Outlet />
     </div>
   );
--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -8,18 +8,18 @@ export default function SidebarWrapper() {
   const goToProcesses = () => navigate("/processes");
 
   return (
-    <Sidebar className="fixed">
+    <Sidebar className="">
       <div className="pt-2">
-        <Sidebar.CTA className="mb-2 !bg-slate-900 p-2 text-center font-mono text-xl font-bold text-blue-950">
+        <Sidebar.CTA className="mb-2 !bg-[#1b2636] p-2 text-center font-mono text-xl font-bold text-white/[.1]">
           REstruct
         </Sidebar.CTA>
         <div className="h-px bg-slate-900/[0.6]" />
         <Sidebar.Items className="border-none">
           <Sidebar.ItemGroup>
-            <Sidebar.Item className="cursor-pointer" onClick={goToHome}>
+            <Sidebar.Item className="cursor-pointer dark:hover:bg-slate-700" onClick={goToHome}>
               Home
             </Sidebar.Item>
-            <Sidebar.Item className="cursor-pointer" onClick={goToProcesses}>
+            <Sidebar.Item className="cursor-pointer dark:hover:bg-slate-700" onClick={goToProcesses}>
               Processes
             </Sidebar.Item>
           </Sidebar.ItemGroup>
