0b622c8564b4788f225a21c9edff5e1464a4d3f2
JJJJJJack <97jack@live.it>
2023-12-23 16:51:14 +0100

    change REstruct style

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -10,12 +10,23 @@ export default function Home() {
     setMsg("Home");
   }, []);
 
+  const REstruct = (
+    <>
+      <span className="inline-block scale-x-[-1] transform select-none text-2xl font-extrabold leading-none text-indigo-900 dark:text-indigo-100">
+        R
+      </span>
+      <span className="inline-block scale-x-[-1] transform select-none text-2xl font-extrabold leading-none text-indigo-900 dark:text-indigo-100">
+        E
+      </span>
+      <span className="inline-block select-none text-lg font-bold leading-none text-indigo-500 dark:text-indigo-400">
+        struct
+      </span>
+    </>
+  );
+
   return (
     <div className="flex h-full items-center">
-      <h1 className="text-3xl font-bold text-white">
-        Welcome to <span className="text-teal-100">RE</span>
-        <span className="text-teal-600">struct</span>
-      </h1>
+      <span className="text-3xl font-bold text-white">Welcome to {REstruct}</span>
     </div>
   );
 }
