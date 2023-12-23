ad294fe60649a25f7dc1c7b42b921eeeb9ed7d88
JJJJJJack <97jack@live.it>
2023-12-23 17:43:36 +0100

    Add Logo component to Sidebar and Home pages

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import { GlobalCtx } from "../../App";
+import Logo from "../../components/Logo/Logo";
 
 export default function Home() {
   const {
@@ -10,23 +11,11 @@ export default function Home() {
     setMsg("Home");
   }, []);
 
-  const REstruct = (
-    <>
-      <span className="inline-block scale-x-[-1] transform select-none text-2xl font-extrabold leading-none text-indigo-900 dark:text-indigo-100">
-        R
-      </span>
-      <span className="inline-block scale-x-[-1] transform select-none text-2xl font-extrabold leading-none text-indigo-900 dark:text-indigo-100">
-        E
-      </span>
-      <span className="inline-block select-none text-lg font-bold leading-none text-indigo-500 dark:text-indigo-400">
-        struct
-      </span>
-    </>
-  );
-
   return (
     <div className="flex h-full items-center">
-      <span className="text-3xl font-bold text-white">Welcome to {REstruct}</span>
+      <span className="text-3xl font-bold text-white">
+        Welcome to <Logo />
+      </span>
     </div>
   );
 }
