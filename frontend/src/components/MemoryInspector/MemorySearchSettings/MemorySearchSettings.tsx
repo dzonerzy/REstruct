0ef41589a9f4d7a1c515b7c7433c956895cf28f6
JJJJJJack <97jack@live.it>
2023-12-28 04:02:17 +0100

    ADD: `Memory Scan Options` section

--- frontend/src/components/MemoryInspector/MemorySearchSettings/MemorySearchSettings.tsx
+++ frontend/src/components/MemoryInspector/MemorySearchSettings/MemorySearchSettings.tsx
@@ -10,6 +10,7 @@ export default function MemorySearchSettings() {
         <button>Next Scan</button>
         <button className="ml-auto">Undo Scan</button>
       </div>
+
       <div className="flex gap-2">
         <section className="mt-auto flex flex-row-reverse gap-1">
           <label className="pb-px" htmlFor="hex-checkbox">
@@ -17,28 +18,55 @@ export default function MemorySearchSettings() {
           </label>
           <input type="checkbox" id="hex-checkbox" />
         </section>
+
         <section className="flex grow flex-col gap-y-[2px]">
           <label htmlFor="value-input">Value:</label>
           <MemoryValueInput scanType={FirstScanType.ExactValue} valueType={ValueType.FourBytes} />
         </section>
       </div>
+
       <section className="grid grid-cols-3 gap-x-2 gap-y-1">
         <div className="col-span-2 flex gap-x-2 justify-self-end">
           <span>Scan&nbsp;Type</span>
           <select className="w-40" name="" id=""></select>
         </div>
+
         <div className="flex gap-1">
-          <input type="checkbox" id="lua-format" />
-          <label htmlFor="lua-formula">lua formula</label>
+          <input type="checkbox" id="lua-formula" />
+          <label htmlFor="lua-formula">Lua&nbsp;formula</label>
         </div>
+
         <div className="col-span-2 flex gap-x-2 justify-self-end">
           <span>Value&nbsp;Type</span>
           <select className="w-40" name="" id=""></select>
         </div>
+
         <div className="flex gap-1">
-          <input type="checkbox" id="lua-format" />
-          <label htmlFor="lua-formula">lua formula</label>
+          <input type="checkbox" id="not" />
+          <label htmlFor="not">Not</label>
         </div>
+
+        <section className="col-span-3 mt-3 grid grid-cols-3 gap-2">
+          <div className="col-span-2 border-[1px] p-1 pt-2">
+            <h3 className="absolute ml-1 mt-[-20px] bg-[#1b2636] px-1">Memory Scan Options</h3>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi delectus commodi quis reiciendis corporis
+              quasi iusto non facere obcaecati accusantium.
+            </div>
+          </div>
+
+          <div className="flex flex-col">
+            <div className="flex h-min gap-1">
+              <input type="checkbox" id="unrandomizer" />
+              <label htmlFor="unrandomizer">Unrandomizer</label>
+            </div>
+
+            <div className="flex h-min gap-1">
+              <input type="checkbox" id="enable-speedhack" />
+              <label htmlFor="enable-speedhack">Enable&nbsp;Speedhack</label>
+            </div>
+          </div>
+        </section>
       </section>
     </search>
   );
